51d9305544a71fd6819bc8a388ab3aad5ccc1105
Cody Rose <rosecodyj@gmail.com>
2019-12-18 00:16:48 -0500

    add elf spawning timer

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,4 +1,21 @@
 extends Node2D
 
+# Time before the game starts
+var initial_startup_time = 3
+# This controls the seconds before another elf is spawned
+# gradually decreases over game lifespan
+export var difficulty = 3
+# Time to be removed from difficulty every spawn
+export var progression = 0.1
+
 func _ready():
-	pass
+	$Timer.start(initial_startup_time)
+
+func _on_Timer_timeout():
+	_spawn_elf()
+	
+	$Timer.start(difficulty)
+	difficulty -= progression
+
+func _spawn_elf():
+	print('spawn_elf')
