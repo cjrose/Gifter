74ad927c8cd20198952ec7218248dcfa3294c7c3
Cody Rose <rosecodyj@gmail.com>
2019-12-17 23:49:45 -0500

    add gift 'throwing'

--- scripts/Present.gd
+++ scripts/Present.gd
@@ -6,7 +6,13 @@ extends RigidBody2D
 # 3 	Yellow
 # default value to avoid errors
 export var present_color = 0
+var move_speed = 0.75
+var moving = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.frame = present_color
+
+func _physics_process(delta):
+	if moving:
+		position += Vector2(move_speed, 0)
