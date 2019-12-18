95dd4f843a0529b3d6169f20fad2f280932d44d1
Cody Rose <rosecodyj@gmail.com>
2019-12-18 12:35:05 -0500

    add elf enemy spawning and despawning

--- scripts/ElfEnemy.gd
+++ scripts/ElfEnemy.gd
@@ -6,9 +6,20 @@ extends RigidBody2D
 # 3 - YELLOW
 var elf_color
 
+# base move speed
+export var base_speed = 0.5
+# amount this move speed is allowed to vary by
+export var difference = 0.25
+
+var move_speed
+var moving = false
 var gifted = false
+var end_x = 0
+
+var rng = RandomNumberGenerator.new()
 
 func _ready():
+	rng.randomize()
 	if elf_color == 0:
 		$AnimatedSprite.play("run_r")
 	if elf_color == 1:
@@ -17,7 +28,15 @@ func _ready():
 		$AnimatedSprite.play("run_b")
 	if elf_color == 3:
 		$AnimatedSprite.play("run_y")
+		
+	end_x = get_parent().get_node("TableStart").position.x
+	move_speed = rng.randf_range(base_speed - difference, base_speed + difference)
 	
 func _physics_process(delta):
 	# TODO add present collision detection
-	pass
+	if moving:
+		self.position += Vector2(-move_speed, 0)
+	
+	if end_x > self.position.x:
+		print('delete elf')
+		self.queue_free()
--- scripts/Game.gd
+++ scripts/Game.gd
@@ -4,13 +4,17 @@ extends Node2D
 var initial_startup_time = 3
 # This controls the seconds before another elf is spawned
 # gradually decreases over game lifespan
-export var difficulty = 3
+export var difficulty = 5
 # Minimum amount of time before elf spawns
-export var min_time = 1
+export var min_time = 3
 # Time to be removed from difficulty every spawn
 export var progression = 0.1
 
+var rng = RandomNumberGenerator.new()
+var elf_enemy_scene = load("res://_scenes/ElfEnemy.tscn")
+
 func _ready():
+	rng.randomize()
 	$Timer.start(initial_startup_time)
 
 func _on_Timer_timeout():
@@ -18,7 +22,34 @@ func _on_Timer_timeout():
 	
 	$Timer.start(difficulty)
 	if difficulty > min_time:
+		print('difficulty', difficulty)
 		difficulty -= progression
 
 func _spawn_elf():
-	pass
+	# pick a random table
+	var table_id = rng.randi_range(0, 3)
+	# pick an elf color
+	var elf_color = rng.randi_range(0, 3)
+	
+	var new_instance = elf_enemy_scene.instance()
+	new_instance.elf_color = elf_color
+	# I hate gdscript / python for not having switch cases
+	if table_id == 0:
+		$Tables/table_0.add_child(new_instance)
+		new_instance.position = $Tables/table_0/TableEnd.position
+		new_instance.moving = true
+	
+	if table_id == 1:
+		$Tables/table_1.add_child(new_instance)
+		new_instance.position = $Tables/table_1/TableEnd.position
+		new_instance.moving = true
+	
+	if table_id == 2:
+		$Tables/table_2.add_child(new_instance)
+		new_instance.position = $Tables/table_2/TableEnd.position
+		new_instance.moving = true
+	
+	if table_id == 3:
+		$Tables/table_3.add_child(new_instance)
+		new_instance.position = $Tables/table_3/TableEnd.position
+		new_instance.moving = true
