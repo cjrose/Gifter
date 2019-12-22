ad2f78eb4186c49db2f49cae5993f4440156f6c8
Cody Rose <rosecodyj@gmail.com>
2019-12-22 01:06:43 -0500

    change difficulty, and win text

--- scripts/GameOver.gd
+++ scripts/GameOver.gd
@@ -8,7 +8,7 @@ var MainMenu = "res://_scenes/Menu.tscn"
 func _ready():
 	$VBoxContainer/PointsLabel.text = "SCORE: " + ("%08d" % global_script.score)
 	if global_script.score >= global_script.point_goal:
-		$VBoxContainer/PointsLabel.text += "\nYOU SAVED CHRISTMAS!"
+		$VBoxContainer/PointsLabel.text += "\nYOU GAVE SANTA ENOUGH PRESENTS\nTO SAVE CHRISTMAS\n\nCONGRATULATIONS!"
 	$VBoxContainer/HBoxContainer2/BackToMenuBtn.grab_focus()
 
 func _on_BackToMenuBtn_pressed():
